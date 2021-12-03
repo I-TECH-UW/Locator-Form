fd72cdbda83e5857b848017e7e652f85da06ddfd
CalebSLane <c.steelelane@gmail.com>
2021-12-02 22:37:09 -0800

    allow longer fields for address

--- locator-form-webapp/src/main/java/org/itech/locator/form/webapp/api/dto/Address.java
+++ locator-form-webapp/src/main/java/org/itech/locator/form/webapp/api/dto/Address.java
@@ -6,18 +6,18 @@ import lombok.Data;
 
 @Data
 public class Address {
-	@Size(max = 50)
+	@Size(max = 255)
 	private String hotelName;
-	@Size(max = 50)
+	@Size(max = 255)
     private String numberAndStreet;
-	@Size(max = 20)
+	@Size(max = 255)
     private String apartmentNumber;
-	@Size(max = 50)
+	@Size(max = 255)
     private String city;
-	@Size(max = 50)
+	@Size(max = 255)
     private String stateProvince;
-	@Size(max = 50)
+	@Size(max = 255)
     private String country;
-	@Size(max = 20)
+	@Size(max = 255)
     private String zipPostalCode;
 }
