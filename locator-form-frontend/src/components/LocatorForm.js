d61ab86b2e30e6395fa28724f5e215630fc88688
CalebSLane <c.steelelane@gmail.com>
2021-12-30 14:04:38 -0700

    add tawkto the passenger form

--- locator-form-frontend/src/components/LocatorForm.js
+++ locator-form-frontend/src/components/LocatorForm.js
@@ -85,6 +85,20 @@ class LocatorForm extends React.Component {
     }
   }
 
+  componentDidMount() {
+    var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/61cd5b53c82c976b71c413d4/1fo52pg1a';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+  }
+
   onSubmitStep = () => {
     return steps.length - 1 === this.state.activeStep;
   }
