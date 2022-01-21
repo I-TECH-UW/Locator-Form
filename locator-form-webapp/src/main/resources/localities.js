51b89bd4ab245dbc2b4d0309027e6adbf6a7e80e
mozzy11 <mozzymutesa@gmail.com>
2022-01-21 16:58:51 +0300

    Add new health office and locality

--- locator-form-webapp/src/main/resources/localities.js
+++ locator-form-webapp/src/main/resources/localities.js
@@ -68,5 +68,10 @@
       "label": "St Pierre Health Office",
       "value": "St Pierre Health Office",
       "list": ["Alma", "Bagatelle", "Beau Bois", "Belle Rive", "Camp Thorel", "Dagotiere", "Dubreuil", "Esperance ( Quartier Militaire)", "GRNW (part)", "Hermitage", "L'Assurance", "L'Avenir", "La Laura-Malenga", "La Pipe", "Melrose", "Moka", "Montagne Blanche", "Montagne Ory", "Monetebello", "Nouvelle Decouverte", "Pailles", "Petit Paquet", "Petit Verger", "Providence", "Quartier Militaire", "Reduit", "Ripailles", "Salazie", "Soreze", "St Pierre", "Valetta", "Verdun", "Vuillemin"]
+    },
+    {
+      "label": "Rodrigues Health Office",
+      "value": "Rodrigues Health Office",
+      "localities": ["Rodrigues"]
     }
   ]
